ebd97666d2ad96f31e2bb725743b479d0641b478
BuddhiniLS <buddhinilasanthika1994@gmail.com>
2025-02-16 22:59:32 +0530

    creat home page🏠

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,19 +1,14 @@
 
-import{Link} from'react-router-dom';
-import'./Homepage.css';
 
+import React from "react";
+import "./homePage.css";
 
-export default function Homepage() {
+export default function HomePage() {
   return (
-    <div className="homepage-container">
-      
-        <h1 >Welcome to Our Website</h1>
-        <p >Explore a world of possibiliies with your services.we are here to make your life easire and more enjoyable.</p>
-      
-          <button onClick={()=>alert('button Clicked!')}>Get started</button>
-          <Link to="/login">Login</Link>  
-      
+    <div className="homepage">
+      <h1>Welcome to Our Website</h1>
+      <p>Your one-stop solution for all your needs.</p>
+      <button>Get Started</button>
     </div>
   );
 }
-
