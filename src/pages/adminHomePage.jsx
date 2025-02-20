42b12aba45a97a3fc3dd819c321b0c274c94cf7e
BuddhiniLS <buddhinilasanthika1994@gmail.com>
2025-02-20 22:07:32 +0530

    adminHomePage Route

--- src/pages/adminHomePage.jsx
+++ src/pages/adminHomePage.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import React from "react";
 import { BsGraphUp, BsBoxSeam, BsCartCheck, BsPeople } from "react-icons/bs";
+import { BrowserRouter, Route, Routes, } from 'react-router-dom'
+import AdminProductsPage from "./Admin/adminProductsPage";
 
 export default function AdminHomePage() {
     return (
@@ -23,7 +25,16 @@ export default function AdminHomePage() {
 
             {/* Main Content */}
             <div className="w-[80%] h-screen bg-red-600 flex justify-center items-center">
-                
+                <h1 className="text-white text-3xl">Welcome to Admin Panel</h1>
+
+                <Routes path="/*" >
+            <Route path="/ Dashboard"element={<h1>Dashboard</h1>} />
+            <Route path="/ products"element={<AdminProductsPage/>} />
+            <Route path="/ orders"element={<h1>orders</h1>} />
+            <Route path="/ customers"element={<h1>customers</h1>} />
+            <Route path="/*" element={<h1>404 not found the admin page</h1>}/>
+            </Routes>
+
             </div>
         </div>
     );
